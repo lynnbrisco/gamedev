cab97d8f02d36f88a5b1059f4f8cd87991ca6fb3
lynn <nendalynn.b538@stu.nebo.edu>
2021-09-15 09:20:04 -0600

    messing around with collision code

---
+++ prototype 1 - tank sim/Assets/scripts/Collision.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ignore this one mr powell, i'm messing around
+
+public class OnCollisionEnter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
