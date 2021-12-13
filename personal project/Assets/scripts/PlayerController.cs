04837e7337fb630b9abf2f85cd29ba1063dfc33e
lynn <nendalynn.b538@stu.nebo.edu>
2021-12-13 09:12:16 -0700

    messing with personal project

--- personal project/Assets/scripts/PlayerController.cs
+++ personal project/Assets/scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //components
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //change rb velocity for floaty jump
+        float dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * 4.5f, rb.velocity.y);
+
+        //jump if spacebar is hit
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 7f);
+        }
     }
 }
